d08f27303d2a7c1410d6b7c21f16cf9e4826ed30
Voided <jurianvierbergen@outlook.com>
2023-01-14 02:40:07 +0100

    wally support
    this is not tested yet though technically it should function as wally requires "src" to be a script in order to properly, to my knowledge at least.
    
    i've only added an init so the entire repo structure remains as desired, alternatively you can move all the files under src and remove the init I've created.

---
+++ src/init.lua
@@ -0,0 +1,6 @@
+-- This is solely for wally support while keeping the repository structure as desired.
+-- Wally requires src to contain an init.lua to reference to so this should fix wally usage.
+
+-- This file can be ignored otherwise.
+
+return require(script.Parent.SmartBone)
